b2fdd81dbed94f924f48a95a73df2800d52eaff7
Lais  Trevizan Domiciano <laistdomiciano@gamil.com>
2024-06-11 16:10:23 +0200

    Designing the Routes and Data Structure

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
+
 
 app = Flask(__name__)
 
+def load_posts():
+    """Opens the json file and loads blog posts"""
+    with open("blog_posts.json", "r") as fileobj:
+        blog_posts = json.load(fileobj)
+    return blog_posts
+
+posts = load_posts()
+
+def save_posts(blog_posts):
+    """Updates the json file"""
+    with open("blog_posts.json", "w") as fileobj:
+        json.dump(blog_posts, fileobj)
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World! Here is Lais, your favourite coder!'
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World! Here is Lais, you favourite coder!'
-
+def index():
+    blog_posts = posts
+    return render_template('index.html', posts=blog_posts)
 
 if __name__ == '__main__':
     app.run()
