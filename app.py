e418dbba2564922e5cd04fabf4eb11cbca655966
laistdomiciano <laistdomiciano@gmail.com>
2024-06-16 09:29:21 +0200

    Adding Add route

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
-
 
 app = Flask(__name__)
 
@@ -10,12 +9,11 @@ def load_posts():
         blog_posts = json.load(fileobj)
     return blog_posts
 
-posts = load_posts()
 
-def save_posts(blog_posts):
+def save_blog_posts(posts):
     """Updates the json file"""
-    with open("blog_posts.json", "w") as fileobj:
-        json.dump(blog_posts, fileobj)
+    with open('blog_posts.json', 'w') as fileobj:
+        json.dump(posts, fileobj)
 
 @app.route('/hello')
 def hello_world():
@@ -24,8 +22,40 @@ def hello_world():
 
 @app.route('/')
 def index():
-    blog_posts = posts
+    blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # Get form data
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        # Load existing blog posts
+        blog_posts = load_posts()
+
+        # Create new blog post
+        new_post = {
+            'id': max(post['id'] for post in blog_posts) + 1 if blog_posts else 1,
+            'author': author,
+            'title': title,
+            'content': content
+        }
+
+        # Append new post to the list
+        blog_posts.append(new_post)
+
+        # Save updated list to JSON file
+        save_blog_posts(blog_posts)
+
+        # Redirect to the home page
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
+
 
 if __name__ == '__main__':
     app.run()
