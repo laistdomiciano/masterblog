c3b52db0b2c984835bd5cc62c4d4e70b14898a18
laistdomiciano <laistdomiciano@gmail.com>
2024-06-16 15:43:42 +0200

    Designing the Update Form

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 
 app = Flask(__name__)
 
+
 def load_posts():
     """Opens the json file and loads blog posts"""
     with open("blog_posts.json", "r") as fileobj:
@@ -15,6 +16,14 @@ def save_blog_posts(posts):
     with open('blog_posts.json', 'w') as fileobj:
         json.dump(posts, fileobj)
 
+
+def fetch_post_by_id(post_id):
+    """Fetch blog posts per ID"""
+    blog_posts = load_posts()
+    for post in blog_posts:
+        if post['id'] == post_id:
+            return post
+    return None
 
 @app.route('/hello')
 def hello_world():
@@ -75,31 +84,37 @@ def delete(post_id):
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
 def update(post_id):
     # Fetch the blog posts from the JSON file
-    blog_posts = load_posts()
-    if blog_posts is None:
+    post = fetch_post_by_id(post_id)
+
+    if post is None:
         # Post not found
         return "Post not found", 404
 
     if request.method == 'POST':
         # Update the post in the JSON file
-        blog_posts['author'] = request.form.get('author')
-        blog_posts['title'] = request.form.get('title')
-        blog_posts['content'] = request.form.get('content')
+        post['author'] = request.form.get('author')
+        post['title'] = request.form.get('title')
+        post['content'] = request.form.get('content')
 
-        # Save updated post back to JSON file
+        # Fetch all posts
         blog_posts = load_posts()
+
+        # Update the post in the list
         for idx, p in enumerate(blog_posts):
             if p['id'] == post_id:
-                blog_posts[idx] = blog_posts
+                blog_posts[idx] = post
                 break
+
+        # Save updated list to JSON file
         save_blog_posts(blog_posts)
 
-        # Redirect back to index page
+        # Redirect back to the home page
         return redirect(url_for('index'))
 
     # Else, it's a GET request
     # Display the update.html page
-    return render_template('update.html', post=blog_posts)
+    return render_template('update.html', post=post)
+
 
 if __name__ == '__main__':
     app.run()
